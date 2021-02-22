bf2b633f4ceee3c199bcaa19040b9684623cd001
Hanna-Syniavska <annarybalko34@gmail.com>
2021-02-22 17:23:45 +0200

    finish booked field

--- script.js
+++ script.js
@@ -1,30 +1,47 @@
 const bookingForm = document.querySelector("#booking-form");
 const bookingButton = document.querySelector("#booking-button");
+const hall = document.querySelector(".hall");
+const bookedField = document.querySelector('.booked');
 
-function getChosenSeats () {
-  let checkedSeats = document.querySelectorAll('.seats__input:checked');
-  for (seat of checkedSeats) {
-    let checkedSeat = seat.value;
-    const [ rowNum, seatNum ] = checkedSeat.split('-');
-    console.log(rowNum, seatNum);
+const price = '$10';
+
+function setBookedField ( {target} ) {
+  if (!target.classList.contains('seats__input')) {
+    return;
   }
-}
+  const [ rowNum, seatNum ] = target.value.split('-');
+  const chosenDate = document.querySelector('.input-date:checked').value;
+  const chosenTime = document.querySelector('.input-time:checked').value;
 
-function getChosenDate () {
-  let checkedDate = document.querySelector('.schedule__date > .schedule__item > .schedule__input:checked').value;
-  console.log(checkedDate);
-}
+  const chosenTicket = `
+  <div class="booked__item">
+      <p class="booked__seat-row">Row: <span class="highlight-text">${rowNum}</span></p>
+      <p class="booked__seat">Seat: <span class="highlight-text">${seatNum}</span></p>
+      <p class="booked__price">Price: <span class="highlight-text">${price}</span></p>
+    </div>`;
 
-function getChosenTime () {
-  let checkedTime = document.querySelector('.schedule__time > .schedule__item > .schedule__input:checked').value;
-  console.log(checkedTime);
+  const bookedTemplate = `<div class="booked__container">
+    <h4 class="booked__title">Your booking:</h4>
+    <p class="booked__date">
+      Date: <span class="highlight-text">${chosenDate}</span>
+    </p>
+    <p class="booked__time">
+      Time: <span class="highlight-text">${chosenTime}</span>
+    </p>
+    ${chosenTicket}
+  </div>`;
+
+  if (!bookedField.hasChildNodes()) {
+    bookedField.innerHTML = bookedTemplate;
+  } else {
+    const bookedContainer =  document.querySelector('.booked__container');
+    bookedContainer.insertAdjacentHTML('beforeend', chosenTicket);
+  }
 }
 
 function sendForm (e) {
   e.preventDefault();
-  getChosenSeats();
-  getChosenDate ();
-  getChosenTime ();
 }
 
+hall.addEventListener('click', setBookedField);
 bookingButton.addEventListener('click',  sendForm);
